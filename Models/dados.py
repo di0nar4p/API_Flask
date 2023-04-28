d56331c5a1a4dcad29f394a9fb642d044b3e098f
di0nar4p <glaucoplay@live.com>
2023-04-28 19:38:31 -0300

    Dicionário temporário

---
+++ Models/dados.py
@@ -0,0 +1,21 @@
+dados= [
+    {   'id': 0,
+        'nome': 'Glauco Pereira Santos',
+        'idade': 29,
+        'nacionalidade': 'Brasileiro',
+        'naturalidade': 'Soteropolitano',
+        },
+    
+    {   'id': 1,
+        'nome': 'Ana Tereza Pereira Santos',
+        'idade': '52',
+        'nacionalidade': 'Brasileira',
+        'naturalidade': 'Soteropolitana',
+        },
+    {   'id': 2,
+        'nome': 'Pedro Raimundo de Pinho Santos',
+        'idade': '?',
+        'nacionalidade': 'Brasileiro',
+        'naturalidade': 'Soteropolitano',
+        }
+]
