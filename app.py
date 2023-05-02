db57de1ec4d94895dba5e041f88f14d602121581
di0nar4p <glaucoplay@live.com>
2023-05-02 08:15:16 -0300

    Demais alterações

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, render_template
-from Models.dados import dados
-
+from flask import Flask, url_for, request, jsonify, render_template, abort, redirect
+from Controller.requests import select_all
+from Controller.conexao import db_connect
 from templates import *
 
 app = Flask(__name__)
@@ -22,42 +22,48 @@ def index():
     
 
 
-@app.route('/dados', methods=['GET'])
+""" @app.route('/dados', methods=['GET'])
 def getAll():
-    
-    return jsonify(dados)
+    senha = request.form['senha']
+    user = request.form['login']
+    dados = select_all('root','5550123Pl@y')
+    return jsonify(dados) """
 
 
-@app.route('/dados/<int:id>', methods=['GET'])
+""" @app.route('/dados/<int:id>', methods=['GET'])
 def getId(id):
+    dados = select_all()
     for dado in dados:
         if dado.get('id') == id:
             return jsonify(dado)
-        
+ """        
 
-@app.route('/dados/<int:id>', methods=['PUT'])
+""" @app.route('/dados/<int:id>', methods=['PUT'])
 def update(id):
     alteracao = request.get_json()
+    dados = select_all()
     for indice,dado in enumerate(dados):
         if dado.get('id') == id:
           dados[indice].update(alteracao)
-          return jsonify(dados[indice])
+          return jsonify(dados[indice]) """
 
 
-@app.route('/dados',methods=['POST'])
+""" @app.route('/dados',methods=['POST'])
 def include():
     new = request.get_json()
+    dados = select_all()
     dados.append(new)
-    return jsonify(dados)
+    return jsonify(dados) """
     
 
-@app.route('/dados/<int:id>',methods=['DELETE'])
+""" @app.route('/dados/<int:id>',methods=['DELETE'])
 def delete(id):
+    
     for indice,dado in enumerate(dados):
         if dado.get('id') == id:
             del dados[indice]
     
-    return jsonify(dados)
+    return jsonify(dados) """
 
 
 @app.route('/', methods=['GET'])
@@ -68,9 +74,14 @@ def index():
 
 @app.route('/home', methods=['GET','POST'])
 def main():
-    
-    return render_template('main.html', dados=dados)
-    
+    senha = request.form['senha']
+    user = request.form['login']
+    if db_connect(user,senha):
+        dados= select_all(user,senha)
+        response= dados
+        return render_template('main.html', dados=response)
+
+        
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
