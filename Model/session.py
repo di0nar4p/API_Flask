96358ac874367154ce5c41510c57d5dd263cb7a3
di0nar4p <glaucoplay@live.com>
2023-05-04 16:26:48 -0300

    correções

--- Model/session.py
+++ Model/session.py
@@ -16,17 +16,3 @@ class Session():
         self.user = {'username':args.user["username"], 'password': args.user["password"]} 
         self.session.append(self.user)
              
-    
-admin = User('root', 'admin')    
-teste = User('Glauco','123')
-teste2 = User('Ana','321')
-
-
-sessao = Session()
-
-sessao.setSession(admin)
-sessao.setSession(teste2)
-sessao.setSession(teste)
-
-
-print(sessao.session)
