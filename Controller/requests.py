c049508342b321c672f49c394eff3f4a1602d708
di0nar4p <glaucoplay@live.com>
2023-05-14 12:53:06 -0300

    Criada requisições de login e alteração no banco

--- Controller/requests.py
+++ Controller/requests.py
@@ -40,9 +40,44 @@ def select_all()-> list:
                 'naturalidade': row[4]
                             }
         request.append(dados)
+        
     cursor.close() 
     return request
 
 
+def updateId(id:int,nome:str,idade:int,nacionalidade:str,naturalidade:str)-> list:
+    query= (f'''UPDATE dados SET
+            nome= "{nome}",
+            idade= "{idade}",
+            nacionalidade= "{nacionalidade}",
+            naturalidade= "{naturalidade}"
+            WHERE id = {id}''')
+    login,password= local()
+    conn = db_connect(login,password)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    request=[]
+    cursor.close() 
+    return request
 
 
+def login(login:str,password:str)->bool:
+    query= ('SELECT * FROM users')
+    remote1,remote2= local()
+    conn = db_connect(remote1,remote2)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    credentials= cursor.fetchall()
+    acesso=[]
+    for credencial in credentials:
+        acesso.append(list(credencial))
+        if login == acesso[0][0] and password == acesso[0][1]:
+            cursor.close()
+            return True
+            
+        else:
+            return False
+
+            
+ 
